3cb62e8104e347a7126527da11e584fdcd728245
vishnu_shanz <166441209+vishnu-shanz@users.noreply.github.com>
2025-01-31 14:50:30 +0530

    Add files via upload

---
+++ Stackimplementation/Main.java
@@ -0,0 +1,16 @@
+package Stackimplementation;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        s.display();
+        s.pop();
+        s.peak();
+        s.display();
+    }
+}
---
+++ Stackimplementation/Stack.java
@@ -0,0 +1,38 @@
+package Stackimplementation;
+
+public class Stack {
+    int a[]=new int[5];
+    int top=-1;
+    public void push(int data){
+        if(top==a.length){
+            System.out.println("Stack is full");
+        }
+        else{
+            top++;
+            a[top]=data;
+        }
+    }
+    public void pop(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+        }
+        else{
+            top--;
+        }
+    }
+    public void peak(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+        }
+        else{
+            System.out.println("Top element:"+a[top]);
+        }
+    }
+    public void display(){
+        System.out.println("Displaying elements:");
+        for(int i=top;i>=0;i--){
+            System.out.println(a[i]);
+        }
+    }
+}
+
